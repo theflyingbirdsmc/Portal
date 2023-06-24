7a4bf8118d631359e1e348a337536641159669f7
Fuglen <fuglenbusiness@gmail.com>
2023-06-24 21:36:06 +0000

    Changed to multi-org to get namespace

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,7 +2,7 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { GithubOrgEntityProvider, defaultUserTransformer } from '@backstage/plugin-catalog-backend-module-github';
+import { GithubMultiOrgEntityProvider, defaultUserTransformer } from '@backstage/plugin-catalog-backend-module-github';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -10,9 +10,10 @@ export default async function createPlugin(
   const builder = await CatalogBuilder.create(env);
   // The org URL below needs to match a configured integrations.github entry
   // specified in your app-config.
-  const githubOrgEntityProvider = GithubOrgEntityProvider.fromConfig(env.config, {
+  const githubOrgEntityProvider = GithubMultiOrgEntityProvider.fromConfig(env.config, {
     id: 'production',
-    orgUrl: 'https://github.com/theflyingbirdsmc',
+    githubUrl: 'https://github.com',
+    orgs: ['theflyingbirdsmc'],
     logger: env.logger,
     schedule: env.scheduler.createScheduledTaskRunner({
       frequency: { minutes: 60 },

6eaef72aa9a341cc244a6623e7bfa3960e4541d4
Fuglen <simon0210@hotmail.dk>
2024-04-19 22:04:57 +0000

    Fix login

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -45,14 +45,13 @@ const app = createApp({
     SignInPage: props => (
       <SignInPage
         {...props}
-        providers={[
-          {
-            id: 'github-auth-provider',
-            title: 'GitHub',
-            message: 'Sign in using GitHub',
-            apiRef: githubAuthApiRef,
-          },
-        ]}
+        auto
+        provider={{
+          id: 'github-auth-provider',
+          title: 'GitHub',
+          message: 'Sign in using GitHub',
+          apiRef: githubAuthApiRef,
+        }}
       />
     ),
   },

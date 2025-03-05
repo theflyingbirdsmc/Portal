67fd2ebd5b9d09bc7662ab19ab16ed781731bbb8
Simon Fugl <fuglenbusiness@gmail.com>
2025-03-05 15:07:38 +0100

    Update main.tf

--- .devcontainer/portal-dev/main.tf
+++ .devcontainer/portal-dev/main.tf
@@ -248,7 +248,7 @@ resource "kubernetes_pod" "main" {
     }
     container {
       name              = "portal"
-      image             = "registry.theflyingbirds.net/repository/docker/backstage-dev:latest"
+      image             = "docker.theflyingbirds.net/backstage-dev:latest"
       image_pull_policy = "Always"
       command           = ["sh", "-c", coder_agent.main.init_script]
       port {
